d9cd2dcffa8a8ff637256329f6e9dc23501ec402
Ove Haugvaldstad <ovewh@login-2.saga.sigma2.no>
2020-02-21 11:23:31 +0100

    added makefile

--- src/par_mod.f90
+++ src/par_mod.f90
@@ -151,13 +151,14 @@ module par_mod
   ! ECMWF
 ! integer,parameter :: nxmax=361,nymax=181,nuvzmax=92,nwzmax=92,nzmax=92,nxshift=359 ! 1.0 degree 92 level
 !  integer,parameter :: nxmax=361,nymax=181,nuvzmax=138,nwzmax=138,nzmax=138,nxshift=0 ! 1.0 degree 138 level
+  integer,parameter :: nxmax=501,nymax=234,nuvzmax=138,nwzmax=138,nzmax=138,nxshift=0 ! 1.0 degree 138 level
 !   integer,parameter :: nxmax=361,nymax=181,nuvzmax=138,nwzmax=138,nzmax=138,nxshift=359 ! 1.0 degree 138 level
 ! integer,parameter :: nxmax=721,nymax=361,nuvzmax=138,nwzmax=138,nzmax=138,nxshift=359  ! 0.5 degree 138 level
 !  integer,parameter :: nxmax=181,nymax=91,nuvzmax=92,nwzmax=92,nzmax=92,nxshift=0  ! CERA 2.0 degree 92 level
 
 ! GFS
-   integer,parameter :: nxmax=361,nymax=181,nuvzmax=138,nwzmax=138,nzmax=138
-   integer :: nxshift=0 ! shift not fixed for the executable 
+!   integer,parameter :: nxmax=361,nymax=181,nuvzmax=138,nwzmax=138,nzmax=138
+!   integer :: nxshift=0 ! shift not fixed for the executable 
 
 
   !*********************************************
@@ -200,7 +201,7 @@ module par_mod
   !**************************************
 
   !integer,parameter :: maxageclass=1,maxzgrid=10,nclassunc=1
-  integer,parameter :: maxageclass=1,nclassunc=1
+  integer,parameter :: maxageclass=50,nclassunc=1
 
   ! nclassunc               number of classes used to calculate the uncertainty
   !                         of the output
@@ -222,8 +223,8 @@ module par_mod
   ! Maximum number of particles, species, and similar
   !**************************************************
 
-  integer,parameter :: maxpart=100000
-  integer,parameter :: maxspec=1
+  integer,parameter :: maxpart=10000000
+  integer,parameter :: maxspec=20
 
   real,parameter :: minmass=0.0001
 
@@ -266,7 +267,7 @@ module par_mod
   ! Dimension of random number field
   !*********************************
 
-  integer,parameter :: maxrand=1000000
+  integer,parameter :: maxrand=100000000
 
   ! maxrand                 number of random numbers used
   
